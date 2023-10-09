ffc5d7a1377816515f41b910c5e6a13d1d92f35e
Daniel Ndanema <dannytech001@gmail.com>
2023-10-10 01:06:22 +0800

    Added Animated Text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,27 @@
+import { useState, useEffect } from "react";
 import "./Hero.css";
 
+const names = ["Daycare", "Preparatory", "Primary", "High"];
+
 function Hero() {
+  const [nameIndex, setNameIndex] = useState(0);
+  const [animationClass, setAnimationClass] = useState("drop-in");
+
+  useEffect(() => {
+    // Code to run on component mount
+    const interval = setInterval(() => {
+      setAnimationClass("drop-out");
+      setTimeout(() => {
+        setNameIndex((prevIndex) => (prevIndex + 1) % names.length);
+        setAnimationClass("drop-in");
+      }, 500);
+    }, 5000);
+    return () => {
+      // Code to run on component unmount
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <section className="hero-section ">
       <div className="hero-container">
@@ -8,7 +29,14 @@ function Hero() {
           <h1 className="heading">
             <span>Welcome to</span>
             <br />
-            <span className="school-title">Gateway Pre-School</span>{" "}
+            <span className="school-title">
+              Gateway{" "}
+              <span className={`animated-text ${animationClass}`}>
+                {names[nameIndex]}
+              </span>
+              <br />
+              School
+            </span>{" "}
           </h1>
           {/* <p>{description}</p> */}
           <button className="btn">Apply Now</button>
